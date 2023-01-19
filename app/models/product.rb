746078320e5968a6021cca09d05a031bcc8ca441
Yuri Barros Lima <yuri.arudapar@yahoo.com>
2023-01-19 11:41:01 -0300

    Mostrar um Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 5 }
+    validates :model, presence: true, length: { minimum: 5 }
+    validates :serial, presence: true, length: { minimum: 5 }
 end
